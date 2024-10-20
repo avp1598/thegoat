32d01d081344ad45ae993d6f957211199ab3dcd6
Aditya Veer Parmar <nakulparmar15@gmail.com>
2024-10-20 18:23:55 +0530

    add image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: "s3.amazonaws.com", protocol: "https" }],
+    domains: ["s3.amazonaws.com"],
   },
 };
 
